246b6d04432f790c4ce441bb9c605309865266b9
Penn M Diba <pdiba@oxbowschool.org>
2020-05-29 19:16:17 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -7,8 +7,9 @@ def nyc_pigeon_organizer(data)
           final_array[name] = {}
         end
         if !final_array[name][key]
-          final_array[name][key] = {}
+          final_array[name][key] = []
         end
+        
       end
     end  
     final_array
