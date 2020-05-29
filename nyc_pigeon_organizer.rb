d08eea4866bb23f25ac90d42555bb1a9585dec5b
Penn M Diba <pdiba@oxbowschool.org>
2020-05-29 19:09:14 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,7 +2,9 @@ def nyc_pigeon_organizer(data)
   # write your code here!
   data.each_with_object({}) do |(key, value), final_array|
     value.each do |inner_key, names|
-      
+      names.each do |name|
+        
+      end
     end  
     final_array
     
